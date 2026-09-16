f5dbc0048786e1f9f3af0ea21477afd60aadc3ed
Annika Deutsch <xgw5dw@virginia.edu>
2026-09-16 14:57:07 -0400

    streamline functionality in eliminating.py

--- Binary_Pulsar_Distances/__init__.py
+++ Binary_Pulsar_Distances/__init__.py
@@ -1,21 +1,31 @@
 """PSRmatch: cross-match pulsar catalogues against optical surveys."""
 
+from .atnf import read_atnf_long_with_errors
 from .eliminating import (
-    check_binary,
-    check_in_globular,
-    check_pos_uncertainty,
+    add_dm_distance,
+    add_gaia_distance,
+    confirm_proper_motion,
+    filter_binary,
+    filter_in_globular,
+    filter_position_uncertainty,
     get_matches,
     matching_pipeline,
     pretty_print,
+    pretty_print_matches,
     psr_to_gaia,
 )
 
 __all__ = [
-    "check_binary",
-    "check_in_globular",
-    "check_pos_uncertainty",
+    "add_dm_distance",
+    "add_gaia_distance",
+    "confirm_proper_motion",
+    "filter_binary",
+    "filter_in_globular",
+    "filter_position_uncertainty",
     "get_matches",
     "matching_pipeline",
     "pretty_print",
+    "pretty_print_matches",
     "psr_to_gaia",
+    "read_atnf_long_with_errors",
 ]
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    requirements = [line.strip() for line in f if line.strip()]
+    requirements = [
+        line.strip() for line in f if line.strip() and not line.startswith("#")
+    ]
 
 setup(
     name="psrmatch",
